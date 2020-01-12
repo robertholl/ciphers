4204572a7e037582d81968a591fa24c9ddf6406e
Robert Holl <robert.holl@gmail.com>
2020-01-11 19:09:44 -0500

    Cleanup of ARGV variable assignments

--- caesar_cipher.pl
+++ caesar_cipher.pl
@@ -6,12 +6,9 @@ use Data::Dumper qw(Dumper);
 use List::MoreUtils qw(first_index); 
 #print Dumper \@ARGV;
 
-# Flag to Encrypt (E) or Decrypt (D). 
-my $encrypt = $ARGV[0];
-# Shift key value
-my $shift = $ARGV[1];
-# Text to be encrypted or decrypted
-my $text = $ARGV[2];
+# $ARGV[0] = Flag to Encrypt (E) or Decrypt (D). 
+# $ARGV[1] = Shift key value
+# $ARGV[2] = Text to be encrypted or decrypted
 
 # Array of characters
 my @alphabet = qw(a b c d e f g h i j k l m n o p q r s t u v w x y x .);
@@ -20,7 +17,7 @@ my $length = scalar @alphabet;
 my $index = first_index { $_ eq 'x'} @alphabet;
 print "Index number is: $index \n";
 print "Array length is: $length \n";
-if ($encrypt eq 'E'){
+if ($ARGV[0] eq 'E'){
 	print "Encrypt code here.\n";
 	print "Shift is: $shift.\n";
 	print "Text to encrypt is: $text\n";
